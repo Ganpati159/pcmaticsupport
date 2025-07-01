3195255f27b08bbf76211f213745522bce247780
Ganpati159 <Ankurgupta3598@outlook.com>
2025-07-01 16:28:52 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'pcmaticsupport'
+author = 'Denial'
+release = '1.0'
+ 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+ 
+html_theme = 'alabaster'
+html_static_path = ['_static']   
+ 
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = 'https://play-lh.googleusercontent.com/kqHouhv1J63wRGvIq2P2XYT8s6Q1_9NcOo1bITdyDtNcCPshziMr5ArinVfpwhUbOQ'
